fa29988fb509769273ac4f7f900be790f7fe9d85
Navneet <navneetkumar.html@gmail.com>
2024-12-05 16:26:32 +0530

    fix error in scroll to top

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -2,12 +2,12 @@ import React from 'react';
 import { Outlet } from "react-router-dom";
 import Header from './components/header/Header.jsx'
 import Footer from './components/footer/Footer.jsx'
-// import ScrollToTop from './components/ScrollToTop.jsx';
+import ScrollToTop from './components/ScrollToTop.jsx';
 function Layout(){
   return (
     <>
     <Header/>
-    {/* <ScrollToTop/> */}
+    <ScrollToTop/>
     <Outlet/>
     <Footer/>
     </>

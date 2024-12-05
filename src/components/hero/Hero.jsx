ea3fca0f2d51c8722d899126fe5f79e9389464c4
Navneet <navneetkumar.html@gmail.com>
2024-12-05 21:37:54 +0530

    fix css loading fil error

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import "./Hero.css"; // Custom styles
+import "./Hero.css"
 import HeroCarousel from "./CrouselHero";
 
 

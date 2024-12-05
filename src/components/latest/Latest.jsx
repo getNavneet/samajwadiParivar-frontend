dbee86c441d3df445ba434149fcbd34bc505d4ca
Navneet <navneetkumar.html@gmail.com>
2024-12-05 12:46:19 +0530

    update latest section

--- src/components/latest/Latest.jsx
+++ src/components/latest/Latest.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Latest = () => {
   return (
-    <div className='text-9xl mx-auto'>
+    <div className='text-4xl md:text-9xl mx-auto'>
        Latest Updates will appear here 
     </div>
   );
